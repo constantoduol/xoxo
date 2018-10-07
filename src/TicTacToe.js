45c1d0f947b6f2c11b12baa55d082d08d5957348
constantoduol <oduolconstant@gmail.com>
2018-10-07 21:50:13 +0300

    changes

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,82 +1,240 @@
 import React from 'react';
 
+const DELAY_BEFORE_COMPUTER_MOVE = 1000; //ms
+
 export default class TicTacToe extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-      'turn': 'x',
+      'currentTurn': 'x',
+      'player': 'x',
       'board': [
         [null, null, null], 
         [null, null, null], 
         [null, null, null]
       ]
+    };
+    this.changePlayer = this.changePlayer.bind(this);
+    this.playerMakeMove = this.playerMakeMove.bind(this);
+    this.checkWin = this.checkWin.bind(this);
+  }
+
+  playerMakeMove(row, col){
+    let {currentTurn, player, board} = this.state;
+    if(currentTurn === player && !board[row][col]){
+      board[row][col] = player;
+      this.setState({board: board, 'currentTurn': this.nextTurn()}, () => setTimeout(this.checkWin, 500));
+      this.computerMakeMove();
+    }
+
+  }
+
+  computerPlayer(){
+    let {player} = this.state;
+    return player === 'x' ? 'o' : 'x';
+  }
+
+  computerMakeMove(){
+    setTimeout(() => {
+      console.log("computer move")
+      let {player, board} = this.state;
+      let {row, col} = this.optimalComputerMove();
+      if(row === -1) {
+        this.gameIsOver("xo")
+        return;
+      }
+
+      board[row][col] = this.computerPlayer();
+      this.setState({currentTurn: this.nextTurn(), board}, () => setTimeout(this.checkWin, 500));
+    }, DELAY_BEFORE_COMPUTER_MOVE);
+  }
+
+  optimalComputerMove(){
+    let {board} = this.state;
+    for(let row = 0; row < board.length; row++){
+      for(let col = 0; col < board[row].length; col++){
+        if(!board[row][col]) return {row, col};
+      }
+    }
+    return {row: -1, col: -1};
+  }
+
+  checkWin(){
+    let {board} = this.state;
+    for(let row = 0; row < board.length; row++){
+      for(let col = 0; col < board[row].length; col++){
+
+        let rowValuesEqual = board[0][col] !== null && board[0][col] === board[1][col] && board[1][col] === board[2][col];
+        let colValuesEqual = board[row][0] !== null && board[row][0] === board[row][1] && board[row][1] === board[row][2];
+        let diag1ValuesEqual = board[0][0] !== null && board[0][0] === board[1][1] && board[1][1] === board[2][2];
+        let diag2ValuesEqual = board[0][2] !== null && board[0][2] === board[1][2] && board[1][2] === board[2][2];
+
+        if(rowValuesEqual){
+          this.gameIsOver(board[0][col]);
+        } else if(colValuesEqual){
+          this.gameIsOver(board[row][0]);
+        } else if(diag1ValuesEqual){
+          this.gameIsOver(board[0][0]);
+        } else if(diag2ValuesEqual){
+          this.gameIsOver(board[0][2]);
+        }
+
+        if(rowValuesEqual || colValuesEqual || diag1ValuesEqual || diag2ValuesEqual) return;
+      }
     }
   }
 
+  gameIsOver(winner="x"){
+    if(winner === "xo"){
+      window.alert("Game over! X and O tied.")
+    } else {
+      window.alert(`Game over! ${winner.toUpperCase()} wins!`);
+    }
+    let {player} = this.state;
+    this.restartGame(player);
+  }
+
+  nextTurn(){
+    let {currentTurn} = this.state;
+    return currentTurn === 'x' ? 'o' : 'x';
+  }
+
+  changePlayer(newPlayer){
+    let {player} = this.state;
+    if(this.gameInProgress()){
+      let confirm = window.confirm("Change player and restart game?");
+      if(confirm) this.restartGame(newPlayer);
+      return;
+    }
+  
+    if(player !== newPlayer){
+      this.setState({player: newPlayer, currentTurn: newPlayer})
+    }
+  }
+
+  gameInProgress(){
+    let {board} = this.state;
+    for(let row of board){
+      for(let content of row){
+        if(content) return true
+      }
+    }
+    return false;
+  }
+
+  restartGame(player='x'){
+    this.setState({
+      'board': [
+        [null, null, null], 
+        [null, null, null], 
+        [null, null, null]
+      ],
+      'player': player,
+      'currentTurn': player
+    })
+  }
+
   render() {
-    let {board, turn} = this.state;
+    let {board, currentTurn, player} = this.state;
     return (
       <React.Fragment>
-        <Board board={board}/>
-        <WhosTurn turn={turn}/>
+        <Board 
+          board={board} 
+          playerMakeMove={this.playerMakeMove} 
+          player={player} 
+          currentTurn={currentTurn}/>
+        <WhosTurn 
+          currentTurn={currentTurn} 
+          changePlayer={this.changePlayer}/>
       </React.Fragment>
     );
   }
 }
 
 const Board = (props) => {
-  let {board} = props;
-  let squares = [];
-  let count = 0
-  for(let row of board){
-    for(let content of row){
-      squares.push(<Square content={content} key={count}/>);
-      count++;
+  let {board, playerMakeMove, player, currentTurn} = props;
+  let tableBody = []
+  for(let row = 0; row < board.length; row++){
+    let tableRow = []
+    for(let col = 0; col < board[row].length; col++){
+      tableRow.push(
+        <Square 
+          content={board[row][col]}
+          row={row}
+          col={col}
+          key={row + "_" + col} 
+          player={player}
+          currentTurn={currentTurn}
+          playerMakeMove={playerMakeMove}/>
+      );
     }
+    tableBody.push(<tr key={row} style={styles.row}>{tableRow}</tr>);
   }
 
   return (
-    <div style={styles.board}>
-      {squares}
-    </div>
+    <table style={styles.board}>
+      <tbody>
+        {tableBody}
+      </tbody>
+    </table>
   )
 }
 
 const Square = (props) => {
-  let {content} = props;
-  content = content == null ? '' : content;
+  let {content, playerMakeMove, row, col, player, currentTurn} = props;
+  let style = styles.square
+  if(player === content) {
+    style = Object.assign({}, styles.square, {'color': 'white'})
+  }
   return (
-    <div style={styles.square}>{content}</div>
+    <td 
+      onClick={() => playerMakeMove(row, col)}
+      style={style}>{content}</td>
   )
 } 
 
 const WhosTurn = (props) => {
-  let {turn} = props;
+  let {currentTurn, changePlayer} = props;
+  let defaultStyle = {paddingLeft: 5, cursor: 'pointer'}
+  let xStyle = currentTurn === 'x' ? Object.assign({}, defaultStyle, styles.currentTurn) : defaultStyle;
+  let oStyle = currentTurn === 'o' ? Object.assign({}, defaultStyle, styles.currentTurn) : defaultStyle;
   return (
-    <div>
-      <span>X</span>
-      <span>O</span>
+    <div style={styles.whosTurn}>
+      <span style={xStyle} onClick={() => changePlayer('x')}>X</span>
+      <span style={oStyle} onClick={() => changePlayer('o')}>O</span>
     </div>
   )
 }
 
 const styles = {
-  'board': {
-    display: 'grid',
-    gridColumnGap: 8,
-    gridRowGap: 8,
+  board: {
     backgroundColor: "gray",
-    gridTemplateColumns: "auto auto auto",
-    width: 250,
-    height: 220,
-    fontFamily: "Arial,sans-serif"
+    width: 280,
+    fontFamily: "Arial,sans-serif",
+    tableLayout: "fixed"
   },
-  'square': {
-    backgroundColor: "#BBDEFB",
+  row: {
+    height: 80
+  },
+  square: {
+    backgroundColor: "rgba(187, 222, 251, 0.8)",
     textAlign: "center",
-    padding: "5px 0",
-    fontSize: 50
+    fontSize: 60,
+    cursor: "pointer"
+  },
+  whosTurn: {
+    width: 270,
+    padding: 5,
+    display: 'grid',
+    background: "gray",
+    gridTemplateColumns: "auto auto",
+    fontSize: 30,
+    fontFamily: "Arial, sans-serif"
+  },
+  currentTurn: {
+    background: "#3cb371",
+    color: "white"
   }
 }
 
